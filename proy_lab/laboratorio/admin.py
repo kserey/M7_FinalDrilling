f96719a9c45c19e1715fc5470d3abbc546adb6b3
kserey <irina.serey@gmail.com>
2025-01-29 00:17:53 -0300

    subiendo cambios del admin django

--- proy_lab/laboratorio/admin.py
+++ proy_lab/laboratorio/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Laboratorio, DirectorGeneral, Producto
 
 # Register your models here.
+
+
+@admin.register(Laboratorio)
+class LaboratorioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+    ordering = ('id',)
+
+@admin.register(DirectorGeneral)
+class DirectorGeneralAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'laboratorio')
+    ordering = ('id',)
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'laboratorio', 'f_fabricacion', 'p_costo', 'p_venta')
+    ordering = ('id',)
+    list_filter = ('nombre', 'laboratorio')
