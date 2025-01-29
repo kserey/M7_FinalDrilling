a82db970bb3e49a15e397e30ab2a3079c8135737
kserey <irina.serey@gmail.com>
2025-01-29 18:49:12 -0300

    subiendo pruebas, corren ok

--- proy_lab/laboratorio/models.py
+++ proy_lab/laboratorio/models.py
@@ -18,16 +18,6 @@ class DirectorGeneral(models.Model):
     def __str__(self):
         return self.nombre
 
-class Producto(models.Model):
-    nombre = models.CharField(max_length=255)
-    laboratorio = models.ForeignKey(Laboratorio, on_delete=models.CASCADE)
-    f_fabricacion = models.DateField()
-    p_costo = models.DecimalField(max_digits=12, decimal_places=2)
-    p_venta = models.DecimalField(max_digits=12, decimal_places=2)
-
-    def __str__(self):
-        return f"{self.nombre} ({self.laboratorio.nombre})"
-
 
 def validar_fecha_fabricacion(value):
     if value.year < 2015:
--- proy_lab/laboratorio/tests.py
+++ proy_lab/laboratorio/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Laboratorio
 
-# Create your tests here.
+class LaboratorioModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        """Configura los datos iniciales para las pruebas."""
+        cls.laboratorio = Laboratorio.objects.create(
+            nombre="Laboratorio Test",
+            ciudad="Santiago",
+            pais="Chile"
+        )
+
+    def test_laboratorio_data(self):
+        """Verifica que los datos guardados coincidan con los creados en setUpTestData."""
+        laboratorio = Laboratorio.objects.get(id=self.laboratorio.id)
+        self.assertEqual(laboratorio.nombre, "Laboratorio Test")
+        self.assertEqual(laboratorio.ciudad, "Santiago")
+        self.assertEqual(laboratorio.pais, "Chile")
+
+    def test_laboratorio_url(self):
+        response = self.client.get(reverse('laboratorio_list'))
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_laboratorio_view_reverse(self):
+        """Verifica que reverse encuentra la URL y que la vista usa la plantilla correcta."""
+        response = self.client.get(reverse("laboratorio_update", args=[self.laboratorio.id]))
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "laboratorio/laboratorio_form_update.html")
+        self.assertContains(response, "Laboratorio Test")
