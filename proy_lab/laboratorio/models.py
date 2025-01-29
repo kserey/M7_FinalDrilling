388208217ea4644b81a0be9d6cae6c75c74c890d
kserey <irina.serey@gmail.com>
2025-01-29 00:39:31 -0300

    se modifican campos en Laboratorio y Director General. Se realiza migración y se sube screen con la consulta de migraciones

--- proy_lab/laboratorio/models.py
+++ proy_lab/laboratorio/models.py
@@ -3,17 +3,20 @@ from django.core.exceptions import ValidationError
 from datetime import date
 
 class Laboratorio(models.Model):
-    nombre = models.CharField(max_length=255, unique=True)
+    nombre = models.CharField(max_length=255)
+    ciudad = models.CharField(max_length=255, null=True, blank=True)
+    pais = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.nombre
 
 class DirectorGeneral(models.Model):
     nombre = models.CharField(max_length=255)
+    especialidad = models.CharField(max_length=255, null=True, blank=True)
     laboratorio = models.OneToOneField(Laboratorio, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.nombre} - {self.laboratorio.nombre}"
+        return self.nombre
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=255)
